542726c4880f057b75a941efb45131472cfece0c
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2024-08-12 23:13:13 +0530

    commit-add

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/atharva-portfolio/",
+  base: "/atharva-portfolio/portfolio/",
 });
